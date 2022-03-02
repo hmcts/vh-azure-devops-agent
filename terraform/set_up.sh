7edc17b7d86bf65853ae752119bdfd83d9a4d4ed
lowflying <chriscm.neu@gmail.com>
2022-03-02 16:53:34 +0000

    fixed PAT token!

--- terraform/set_up.sh
+++ terraform/set_up.sh
@@ -15,7 +15,7 @@ su - vhadoagent -c "
   --agent "vh-devops-agent-self-hosted" \
   --url "https://hmctsreform.visualstudio.com" \
   --auth PAT \
-  --token "o2irf5uumopebjm6g7ohxcyaprufklijdnyv5hssgie66faraw2q" \
+  --token "PAT-TOKEN" \
   --pool "vh-self-hosted" \
   --replace \
   --acceptTeeEula & wait $!"
