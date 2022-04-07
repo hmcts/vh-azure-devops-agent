5e42ec68dfa35ee4069f07795e4fc15f442ece85
Thomas Watson <thomas.watson@version1.com>
2022-04-07 09:55:32 +0100

    Changed two terraform files

--- terraform/02-kv.tf
+++ terraform/02-kv.tf
@@ -10,7 +10,7 @@ resource "azurerm_key_vault" "keyvault_ado_agent" {
   resource_group_name         = azurerm_resource_group.vh-devops-agent-rg.name
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
+  # soft_delete_retention_days  = 7
   purge_protection_enabled    = false
 
   sku_name = "standard"
