0103f5234f42d9a744c076a72a9f114f92217fb6
Thomas Watson <thomas.watson@version1.com>
2022-04-13 14:27:54 +0100

    update extension module

--- terraform/03-vm.tf
+++ terraform/03-vm.tf
@@ -51,7 +51,7 @@ resource "azurerm_virtual_machine_extension" "create-agent" {
   publisher            = "Microsoft.Azure.Extensions"
   type                 = "CustomScript"
   type_handler_version = "2.1"
-  virtual_machine_id   = azurerm_virtual_machine.vh-devops-agent-vm.id
+  virtual_machine_id   = azurerm_linux_virtual_machine.vh-devops-agent-vm.id
 
   protected_settings = <<PROTECTED_SETTINGS
       {
