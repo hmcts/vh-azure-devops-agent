90f0083d586f68306f225c0c05c483f5c4ce872f
Thomas Watson <thomas.watson@version1.com>
2022-04-20 17:01:39 +0100

    Diagnostic commits

--- terraform/set_up.sh
+++ terraform/set_up.sh
@@ -19,7 +19,7 @@ su - vhadoagent -c "
   --agent "vh-devops-agent-selfhost-ENV" \
   --url "https://hmctsreform.visualstudio.com" \
   --auth PAT \
-  --token "ajibhlvhl6ptgzeaq2tzwt3me2kuc44xcjo254ytq4yuemreuefq" \
+  --token 'ajibhlvhl6ptgzeaq2tzwt3me2kuc44xcjo254ytq4yuemreuefq' \
   --pool "vh-self-hosted" \
   --replace \
   --acceptTeeEula & wait $!"
