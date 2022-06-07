495386d579ee4e8850aec2a05b5cd1d48d2724c4
Thomas Watson <thomas.watson@version1.com>
2022-06-07 09:56:51 +0100

    parameterise the dev ops org

--- terraform/set_up.sh
+++ terraform/set_up.sh
@@ -11,7 +11,7 @@ sudo tar zxvf ./vsts-agent-linux-x64-$AZP_AGENT_VERSION.tar.gz
 sudo chmod -R 777 /myagent
 #Install
 
-sudo runuser -l vhadoagent -c "cd /myagent ; ./config.sh --unattended --url https://hmctsreform.visualstudio.com --auth pat --token REPLACE --pool vh-self-hosted --agent vh-devops-agent-self-hosted-PL --acceptTeeEula & wait $!"
+sudo runuser -l vhadoagent -c "cd /myagent ; ./config.sh --unattended --url DEVOPSURL --auth pat --token REPLACE --pool vh-self-hosted --agent vh-devops-agent-self-hosted-PL --acceptTeeEula & wait $!"
 sudo /myagent/svc.sh install
 
 # Install .NET CLI dependencies
