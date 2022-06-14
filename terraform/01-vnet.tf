1b910aa2eaa5c476c9fb86e02314fc34af126756
Anish Govind <anish.govind@version1.com>
2022-06-14 11:23:48 +0100

    add

--- terraform/01-vnet.tf
+++ terraform/01-vnet.tf
@@ -12,3 +12,17 @@ resource "azurerm_subnet" "vh-devops-agent-subnet" {
   virtual_network_name = azurerm_virtual_network.vh-devops-agent-vnet.name
   address_prefixes     = [var.subnet_name_vh_agent_address]
 }
+
+resource "azurerm_virtual_network_peering" "agent-to-hubnonprod" {
+  name                      = "agenttohubnonprod"
+  resource_group_name       = azurerm_resource_group.vh-devops-agent-rg.name
+  virtual_network_name      = var.vnet_name
+  remote_virtual_network_id = "/subscriptions/fb084706-583f-4c9a-bdab-949aac66ba5c/resourceGroups/hmcts-hub-nonprodi/providers/Microsoft.Network/virtualNetworks/hmcts-hub-nonprodi"
+}
+
+resource "azurerm_virtual_network_peering" "hubnonprod-to-agent" {
+  name                      = "hubnonprodtoagent"
+  resource_group_name       = "hmcts-hub-nonprodi"
+  virtual_network_name      = "hmcts-hub-nonprodi"
+  remote_virtual_network_id = azurerm_virtual_network.vh-devops-agent-vnet.id
+}
