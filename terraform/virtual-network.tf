38a0dd92ea1d0e0d6dfe3812e574042be2fc875e
Anish Govind <anish.govind@version1.com>
2022-06-20 15:59:58 +0100

    add

--- terraform/virtual-network.tf
+++ terraform/virtual-network.tf
@@ -105,3 +105,24 @@ resource "azurerm_private_dns_zone_virtual_network_link" "vnet_to_dns" {
   virtual_network_id    = azurerm_virtual_network.vh_infra_core_ado.id
   tags                  = local.common_tags
 }
+
+######################################################
+# Route Table & Subnet-RT Link. ##########################################
+
+resource "azurerm_route_table" "agent-rt" {
+  name                          = var.rt_name
+  location                      = var.location
+  resource_group_name           = azurerm_resource_group.vh_infra_core_ado.name
+  disable_bgp_route_propagation = false
+  tags                          = local.common_tags
+
+  dynamic "route" {
+    for_each = var.route_table
+    content {
+      name                   = route.value.name
+      address_prefix         = route.value.address_prefix
+      next_hop_type          = route.value.next_hop_type
+      next_hop_in_ip_address = route.value.next_hop_in_ip_address
+    }
+  }
+}
