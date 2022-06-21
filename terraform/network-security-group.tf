a632c1e6ce1c401f32b1d60c1fe2841636a02c9d
Anish Govind <anish.govind@version1.com>
2022-06-21 15:21:28 +0100

    add

--- terraform/network-security-group.tf
+++ terraform/network-security-group.tf
@@ -5,19 +5,19 @@ resource "azurerm_network_security_group" "vh_infra_core_ado_nsg" {
   tags                = local.common_tags
 }
 
-resource "azurerm_network_security_rule" "DenyVnetInbound" {
-  name                        = "DenyVnetInbound"
-  priority                    = 4096
-  direction                   = "Inbound"
-  access                      = "Deny"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefix       = "VirtualNetwork"
-  destination_address_prefix  = "VirtualNetwork"
-  resource_group_name         = azurerm_resource_group.vh_infra_core_ado.name
-  network_security_group_name = azurerm_network_security_group.vh_infra_core_ado_nsg.name
-}
+# resource "azurerm_network_security_rule" "DenyVnetInbound" {
+#   name                        = "DenyVnetInbound"
+#   priority                    = 4096
+#   direction                   = "Inbound"
+#   access                      = "Deny"
+#   protocol                    = "*"
+#   source_port_range           = "*"
+#   destination_port_range      = "*"
+#   source_address_prefix       = "VirtualNetwork"
+#   destination_address_prefix  = "VirtualNetwork"
+#   resource_group_name         = azurerm_resource_group.vh_infra_core_ado.name
+#   network_security_group_name = azurerm_network_security_group.vh_infra_core_ado_nsg.name
+# }
 
 
 resource "azurerm_subnet_network_security_group_association" "vh_infra_core_ado_nsg" {
