b061b3bad3a8f87e3cc51d4806a2be4e0b40bf02
Anish Govind <anish.govind@version1.com>
2022-07-05 22:14:19 +0100

    add

--- terraform/network-security-group.tf
+++ terraform/network-security-group.tf
@@ -18,7 +18,19 @@ resource "azurerm_network_security_rule" "DenyAllInbound" {
   resource_group_name         = azurerm_resource_group.vh_infra_core_ado.name
   network_security_group_name = azurerm_network_security_group.vh_infra_core_ado_nsg.name
 }
-
+resource "azurerm_network_security_rule" "AllowBastion" {
+  name                        = "AllowBastion"
+  priority                    = 4095
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "22"
+  destination_port_range      = "22"
+  source_address_prefix       = "10.11.8.36/32, 10.11.8.0/23, 10.96.64.0/18"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.vh_infra_core_ado.name
+  network_security_group_name = azurerm_network_security_group.vh_infra_core_ado_nsg.name
+}
 
 resource "azurerm_subnet_network_security_group_association" "vh_infra_core_ado_nsg" {
   network_security_group_id = azurerm_network_security_group.vh_infra_core_ado_nsg.id
