82a8d2c02d0daab2ff974f0cb56346d17fcd4112
Christopher Pateman <christopher.pateman@version1.com>
2022-08-02 10:33:28 +0100

    VIH-9061 - Add multi vm

--- terraform/virtual-machine.tf
+++ terraform/virtual-machine.tf
@@ -1,27 +1,37 @@
+locals {
+  vms = {
+    for item in range(3) :
+    "vh-ado-agent-0${item}" => {
+      name = "vh-ado-agent-0${item}"
+    }
+  }
+}
+
 
 # Create Virtual Machine
-resource "azurerm_network_interface" "vh_ado_agent_01_nic" {
-  name                = "${var.vm_name}-nic"
+resource "azurerm_network_interface" "vh_ado_agent_nic" {
+  for_each = local.vms
+
+  name                = "${each.value.name}-nic"
   location            = azurerm_resource_group.vh_infra_core_ado.location
   resource_group_name = azurerm_resource_group.vh_infra_core_ado.name
 
   ip_configuration {
     name                          = "IpConfig"
     subnet_id                     = azurerm_subnet.vh_infra_core_ado_snet.id
-    private_ip_address_allocation = "Static"
-    private_ip_address            = var.vm_private_ip_address
+    private_ip_address_allocation = "Dynamic"
   }
 
   tags = local.common_tags
 }
 
-resource "azurerm_linux_virtual_machine" "vh_ado_agent_01" {
-  # The tfsec:ignore comment below ignores the tfsec password check as the password is random 
-  # and only generated at runtime and it is needed by the Azure devops agent that is deployed
-  name                  = var.vm_name
+resource "azurerm_linux_virtual_machine" "vh_ado_agent" {
+  for_each = local.vms
+
+  name                  = each.value.name
   location              = azurerm_resource_group.vh_infra_core_ado.location
   resource_group_name   = azurerm_resource_group.vh_infra_core_ado.name
-  network_interface_ids = [azurerm_network_interface.vh_ado_agent_01_nic.id]
+  network_interface_ids = [lookup(azurerm_network_interface.vh_ado_agent_nic, "${each.value.name}-nic", "").id]
   size                  = "Standard_D4s_v3"
 
   disable_password_authentication = false #tfsec:ignore:azure-compute-disable-password-authentication
@@ -29,7 +39,7 @@ resource "azurerm_linux_virtual_machine" "vh_ado_agent_01" {
   admin_password                  = random_password.password.result
 
   os_disk {
-    name                 = var.vm_osdisk_name
+    name                 = "${each.value.name}-os-disk"
     caching              = "ReadWrite"
     storage_account_type = "Premium_LRS"
     disk_size_gb         = 128
@@ -47,11 +57,13 @@ resource "azurerm_linux_virtual_machine" "vh_ado_agent_01" {
 
 
 resource "azurerm_virtual_machine_extension" "AzureDevOpsAgent" {
+  for_each = local.vms
+
   name                 = "AzureDevOpsAgent"
   publisher            = "Microsoft.Azure.Extensions"
   type                 = "CustomScript"
   type_handler_version = "2.1"
-  virtual_machine_id   = azurerm_linux_virtual_machine.vh_ado_agent_01.id
+  virtual_machine_id   = lookup(azurerm_linux_virtual_machine.vh_ado_agent, each.value.name, "").id
 
   protected_settings = <<PROTECTED_SETTINGS
       {
