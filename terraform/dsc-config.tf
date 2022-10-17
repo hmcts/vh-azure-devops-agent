02987bc1d9f051040ab5c1a9b3e4babacd51ef8a
Benjamin Garside (v1) <benjamin.garside@version1.com>
2022-10-17 17:11:02 +0100

    reformat and fix refs

--- terraform/dsc-config.tf
+++ terraform/dsc-config.tf
@@ -11,8 +11,8 @@ resource "azurerm_automation_dsc_configuration" "vh_infra_core_ado" {
 
 resource "azurerm_automation_dsc_nodeconfiguration" "adoagent" {
   name                    = "adoagent.localhost"
-  resource_group_name     = "${azurerm_resource_group.aadscrg.name}"
-  automation_account_name = "${azurerm_automation_account.aa.name}"
+  resource_group_name     = azurerm_resource_group.vh_infra_core_ado.name
+  automation_account_name = azurerm_automation_account.vh_infra_core_ado.name
   depends_on              = ["azurerm_automation_dsc_configuration.vh_infra_core_ado"]
-  content_embedded        = "${file("./localhost.mof")}"
+  content_embedded        = file("./localhost.mof")
 }
