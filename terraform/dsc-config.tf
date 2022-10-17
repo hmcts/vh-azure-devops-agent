fbc614a319d4c756ffe4379970b5b2d845d74941
Benjamin Garside (v1) <benjamin.garside@version1.com>
2022-10-17 17:08:30 +0100

    testing a mof

--- terraform/dsc-config.tf
+++ terraform/dsc-config.tf
@@ -9,10 +9,10 @@ resource "azurerm_automation_dsc_configuration" "vh_infra_core_ado" {
   tags = local.common_tags
 }
 
-# resource "azurerm_automation_dsc_nodeconfiguration" "timezone" {
-#   name                    = "adoagent.localhost"
-#   resource_group_name     = "${azurerm_resource_group.aadscrg.name}"
-#   automation_account_name = "${azurerm_automation_account.aa.name}"
-#   depends_on              = ["azurerm_automation_dsc_configuration.vh_infra_core_ado"]
-#   content_embedded        = "${file("./localhost.mof")}"
-# }
+resource "azurerm_automation_dsc_nodeconfiguration" "adoagent" {
+  name                    = "adoagent.localhost"
+  resource_group_name     = "${azurerm_resource_group.aadscrg.name}"
+  automation_account_name = "${azurerm_automation_account.aa.name}"
+  depends_on              = ["azurerm_automation_dsc_configuration.vh_infra_core_ado"]
+  content_embedded        = "${file("./localhost.mof")}"
+}
