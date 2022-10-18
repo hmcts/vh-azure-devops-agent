d1544eba68910922a9ae8ba888427636510f0667
Benjamin Garside (v1) <benjamin.garside@version1.com>
2022-10-18 12:06:08 +0100

    adding debug to ps1

--- compile-mof.ps1
+++ compile-mof.ps1
@@ -18,6 +18,8 @@ $MOFb16 = Get-ChildItem "$PSScriptRoot\dsc" -Recurse -Include '*.mof'
 mkdir "$PSScriptRoot\dsc\mof\" -Force
 
 foreach ($MOF in $MOFb16) {
-    Write-Output "Converting MOFS: $eachConfiguration"
+    Write-Output "Converting MOFS: $MOF"
     Get-Content $MOF | Set-Content -Encoding utf8 "$PSScriptRoot\dsc\mof\$($MOF.Name)"
 }
+
+Get-ChildItem "$PSScriptRoot\dsc\mof\"
