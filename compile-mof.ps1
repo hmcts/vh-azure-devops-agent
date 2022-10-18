6d3b77ddc0a16878e5fec9b9ea38cd8c4ca49a14
Benjamin Garside (v1) <benjamin.garside@version1.com>
2022-10-18 15:07:55 +0100

    updating compile logic

--- compile-mof.ps1
+++ compile-mof.ps1
@@ -1,27 +1,56 @@
-
 #Set global error action
 $ErrorActionPreference = 'Stop'
 
+function Install-LocalModules {
+    param (
+        $modules
+        )
+        foreach ($module in $modules) {
+            Write-Output "Installing module: $module"
+            Install-Module -Name $module
+        }
+    }
+
+function ConvertTo-MOF {
+    param (
+        $file,
+        $outputDir,
+        $params
+    )
+    Write-Output "Building MOFS: $file"
+    if ($params) {
+        Write-Output "With params"
+        $list = ""
+        foreach ($param in $params.PSObject.Properties) {
+            $list = "$list -$($param.Name) $($param.Value)"
+        }
+        .$file $list
+    } else {
+        Write-Output "Without params"
+        .$file
+    }
+    Write-Output "Converting MOFS: $file"
+    Get-Content $file | Set-Content -Encoding utf8 "$outputDir\$($file.Name)"
+}
+
+# Get configs
+$configs = Get-Content "./dsc/configs.json" | ConvertFrom-Json
+$tfPath = "$PSScriptRoot\terraform"
+$dscPath = "$PSScriptRoot\dsc"
+
 #Delete Pre-existing MOFS in our DSC base folder
-Get-ChildItem "$PSScriptRoot\dsc" -Recurse -Include '*.mof' | Remove-Item -Force
-Get-ChildItem "$PSScriptRoot\dsc" -Recurse -Include '*.mof.error' | Remove-Item -Force
+Get-ChildItem dscPath -Recurse -Include '*.mof' | Remove-Item -Force
+Get-ChildItem dscPath -Recurse -Include '*.mof.error' | Remove-Item -Force
 
-#Build MOFS
-$DSCFiles = Get-ChildItem "$PSScriptRoot\dsc" -Recurse -Include '*.ps1'
+#Loop over each config
+foreach($obj in $configs.Configs.PSObject.Properties) {
+        $file = $obj.Value.file
+        $modules = $obj.Value.modules
+        $name = $obj.Name
+        $params = $obj.Value.params
+        
+        Write-Output "Processing $name"
+        Install-LocalModules -modules $modules
+        ConvertTo-MOF -file "$dscPath\$file" -params $params -outputDir $tfPath
 
-foreach ($eachConfiguration in $DSCFiles) {
-    Write-Output "Building MOFS: $eachConfiguration"
-    .$eachConfiguration
 }
-      
-$MOFb16 = Get-ChildItem "$PSScriptRoot\dsc" -Recurse -Include '*.mof'
-mkdir "$PSScriptRoot\terraform\mof\" -Force
-
-Install-Module -Name 'cChoco'
-
-foreach ($MOF in $MOFb16) {
-    Write-Output "Converting MOFS: $MOF"
-    Get-Content $MOF | Set-Content -Encoding utf8 "$PSScriptRoot\terraform\mof\$($MOF.Name)"
-}
-
-Get-ChildItem "$PSScriptRoot\terraform\mof\"
