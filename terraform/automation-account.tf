a93a05183a53a5797f0b260b553c00c29bb6d1cb
Benjamin Garside (v1) <benjamin.garside@version1.com>
2022-10-19 10:16:54 +0100

    rename auto account

--- terraform/automation-account.tf
+++ terraform/automation-account.tf
@@ -1,6 +1,6 @@
 
 resource "azurerm_automation_account" "vh_infra_core_ado" {
-  name                = "vh-infra-ado-auto"
+  name                = "vh-infra-core-ado"
   location            = azurerm_resource_group.vh_infra_core_ado.location
   resource_group_name = azurerm_resource_group.vh_infra_core_ado.name
   sku_name            = "Basic"
