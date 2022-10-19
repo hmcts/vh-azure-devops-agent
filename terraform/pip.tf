d00e91906b28cb7e3778227768ff829cdc4cff60
Benjamin Garside (v1) <benjamin.garside@version1.com>
2022-10-19 17:05:06 +0100

    typo

--- terraform/pip.tf
+++ terraform/pip.tf
@@ -6,7 +6,7 @@ locals {
 resource "azurerm_public_ip" "dev_pip" {
   for_each = local.pips
 
-  name                = "devPip=${each.name}"
+  name                = "devPip=${each.value.name}"
   resource_group_name = azurerm_resource_group.vh_infra_core_ado.name
   location            = azurerm_resource_group.vh_infra_core_ado.location
   allocation_method   = "Static"
