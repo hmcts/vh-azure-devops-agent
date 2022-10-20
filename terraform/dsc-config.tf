ff0ec32f5ac2e28b78dda06daed74c41643d85aa
Benjamin Garside (v1) <benjamin.garside@version1.com>
2022-10-20 16:03:04 +0100

    removing dsc

--- terraform/dsc-config.tf
+++ terraform/dsc-config.tf
@@ -1,21 +1,21 @@
 
-resource "azurerm_automation_dsc_configuration" "vh_infra_core_ado" {
-  name                    = "SelfHostedAgent"
-  resource_group_name     = azurerm_resource_group.vh_infra_core_ado.name
-  automation_account_name = azurerm_automation_account.vh_infra_core_ado.name
-  location                = azurerm_resource_group.vh_infra_core_ado.location
-  content_embedded        = file("./SelfHostedAgent.ps1")
+# resource "azurerm_automation_dsc_configuration" "vh_infra_core_ado" {
+#   name                    = "SelfHostedAgent"
+#   resource_group_name     = azurerm_resource_group.vh_infra_core_ado.name
+#   automation_account_name = azurerm_automation_account.vh_infra_core_ado.name
+#   location                = azurerm_resource_group.vh_infra_core_ado.location
+#   content_embedded        = file("./SelfHostedAgent.ps1")
 
-  tags = local.common_tags
-}
+#   tags = local.common_tags
+# }
 
-resource "azurerm_automation_dsc_nodeconfiguration" "adoagent" {
-  name                    = "SelfHostedAgent.localhost"
-  resource_group_name     = azurerm_resource_group.vh_infra_core_ado.name
-  automation_account_name = azurerm_automation_account.vh_infra_core_ado.name
-  content_embedded        = file("./localhost.mof")
+# resource "azurerm_automation_dsc_nodeconfiguration" "adoagent" {
+#   name                    = "SelfHostedAgent.localhost"
+#   resource_group_name     = azurerm_resource_group.vh_infra_core_ado.name
+#   automation_account_name = azurerm_automation_account.vh_infra_core_ado.name
+#   content_embedded        = file("./localhost.mof")
 
-  depends_on = [
-    azurerm_automation_dsc_configuration.vh_infra_core_ado
-  ]
-}
+#   depends_on = [
+#     azurerm_automation_dsc_configuration.vh_infra_core_ado
+#   ]
+# }
