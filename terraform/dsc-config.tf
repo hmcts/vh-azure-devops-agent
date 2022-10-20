4fdc7837a61239315be7d8de56bf23c132d3ea80
Benjamin Garside (v1) <benjamin.garside@version1.com>
2022-10-20 15:31:29 +0100

    move dsc ps1

--- terraform/dsc-config.tf
+++ terraform/dsc-config.tf
@@ -4,7 +4,7 @@ resource "azurerm_automation_dsc_configuration" "vh_infra_core_ado" {
   resource_group_name     = azurerm_resource_group.vh_infra_core_ado.name
   automation_account_name = azurerm_automation_account.vh_infra_core_ado.name
   location                = azurerm_resource_group.vh_infra_core_ado.location
-  content_embedded        = file("../dsc/SelfHostedAgent.ps1")
+  content_embedded        = file("./SelfHostedAgent.ps1")
 
   tags = local.common_tags
 }
