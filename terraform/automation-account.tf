53ca7f67f8a1fc0803857dcef45528643ad0bd0b
Benjamin Garside (v1) <benjamin.garside@version1.com>
2022-10-21 11:12:43 +0100

    install module

--- terraform/automation-account.tf
+++ terraform/automation-account.tf
@@ -7,3 +7,13 @@ resource "azurerm_automation_account" "vh_infra_core_ado" {
 
   tags = local.common_tags
 }
+
+resource "azurerm_automation_module" "cChoco" {
+  name                    = "cChoco"
+  resource_group_name     = azurerm_resource_group.vh_infra_core_ado.name
+  automation_account_name = azurerm_automation_account.vh_infra_core_ado.name
+
+  module_link {
+    uri = "https://www.powershellgallery.com/api/v2/package/cChoco/2.5.0.0"
+  }
+}
