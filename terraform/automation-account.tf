5545d99a8f9abf85847508504fb3a99d1e866568
Benjamin Garside (v1) <benjamin.garside@version1.com>
2022-10-21 11:44:56 +0100

    tidy and add DSC to CI

--- terraform/automation-account.tf
+++ terraform/automation-account.tf
@@ -1,4 +1,3 @@
-
 resource "azurerm_automation_account" "vh_infra_core_ado" {
   name                = "vh-infra-core-ado"
   location            = azurerm_resource_group.vh_infra_core_ado.location
@@ -6,14 +5,4 @@ resource "azurerm_automation_account" "vh_infra_core_ado" {
   sku_name            = "Basic"
 
   tags = local.common_tags
-}
-
-resource "azurerm_automation_module" "cChoco" {
-  name                    = "cChoco"
-  resource_group_name     = azurerm_resource_group.vh_infra_core_ado.name
-  automation_account_name = azurerm_automation_account.vh_infra_core_ado.name
-
-  module_link {
-    uri = "https://www.powershellgallery.com/api/v2/package/cChoco/2.5.0.0"
-  }
 }
--- terraform/virtual-machine.tf
+++ terraform/virtual-machine.tf
@@ -11,8 +11,6 @@ locals {
   offer                 = "windows-10"
   sku                   = "win10-21h2-pro-g2"
   version               = "latest"
-  dsc_ConfigurationMode = "ApplyAndAutoCorrect"
-  winscript             = "powershell Set-ExecutionPolicy Bypass -Scope Process -Force; powershell Enable-PsRemoting -SkipNetworkProfileCheck -Force; powershell exit 0"
 }
 
 # Create Virtual Machine
@@ -95,49 +93,4 @@ resource "azurerm_virtual_machine_data_disk_attachment" "data_disk_attachment" {
   virtual_machine_id = azurerm_windows_virtual_machine.vh_ado_agent[each.value.name].id
   lun                = "10"
   caching            = "ReadWrite"
-}
-
-resource "azurerm_virtual_machine_extension" "ps_remoting" {
-  for_each = local.vms
-
-  name                 = "PostDeploymentScript"
-  virtual_machine_id   = azurerm_windows_virtual_machine.vh_ado_agent[each.value.name].id
-  publisher            = "Microsoft.Compute"
-  type                 = "CustomScriptExtension"
-  type_handler_version = "1.9"
-
-  protected_settings = <<PSETTINGS
-    {
-       "commandToExecute": "${local.winscript}"
-    }
-PSETTINGS
-
-  tags = local.common_tags
-
-  depends_on = [
-    azurerm_windows_virtual_machine.vh_ado_agent
-  ]
-}
-
-resource "azurerm_virtual_machine_extension" "AADLoginForWindows" {
-  for_each = local.vms
-
-  name                       = "AADLoginForWindows"
-  virtual_machine_id         = azurerm_windows_virtual_machine.vh_ado_agent[each.value.name].id
-  publisher                  = "Microsoft.Azure.ActiveDirectory"
-  type                       = "AADLoginForWindows"
-  type_handler_version       = "1.0"
-  auto_upgrade_minor_version = true
-
-  depends_on = [
-    azurerm_windows_virtual_machine.vh_ado_agent,
-    azurerm_virtual_machine_extension.ps_remoting
-  ]
-
-}
-
-resource "azurerm_role_assignment" "IAM_VMLogin" {
-  scope                = azurerm_resource_group.vh_infra_core_ado.id
-  role_definition_name = "Virtual Machine Administrator Login"
-  principal_id         = "5c77fdba-1174-4bf3-8faa-934133dac70d" #BG for test
 }
