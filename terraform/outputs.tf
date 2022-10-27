6cc18890cb39016e7f6cf9574e64b517ef8b6829
Benjamin Garside (v1) <benjamin.garside@version1.com>
2022-10-27 09:31:11 +0100

    getting RG from TF

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,3 +3,7 @@ output "tf_vm_names" {
     for vm in azurerm_windows_virtual_machine.vh_ado_agent : vm.name
   ]
 }
+
+output "resource_group_name" {
+  value = azurerm_resource_group.vh_infra_core_ado.name
+}
