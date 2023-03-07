7723f5f30c00eeb22575c15ec3c918e735555ea7
Your Name <james.ferrari@version1.com>
2023-03-07 10:15:49 +0000

    add new pipeline for image build

---
+++ terraform/_data.tf
@@ -0,0 +1,10 @@
+data "azurerm_resource_group" "dns" {
+  name     = "core-infra-intsvc-rg"
+  provider = azurerm.core_infra_dns
+}
+
+data "azurerm_resource_group" "dns_sandbox" {
+  name     = "core-infra-intsvc-rg"
+  provider = azurerm.sandbox_dns
+}
+
