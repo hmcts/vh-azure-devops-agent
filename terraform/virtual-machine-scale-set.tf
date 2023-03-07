59d79d326cd71ea063fdc04b5e5353a8bbe1f8ab
Your Name <james.ferrari@version1.com>
2023-03-07 11:52:16 +0000

    update vmss, amend pipeline task

---
+++ terraform/virtual-machine-scale-set.tf
@@ -0,0 +1,45 @@
+resource "tls_private_key" "vmss" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "azurerm_linux_virtual_machine_scale_set" "vh_ado_agent_vmss" {
+  name                = var.vmss_name
+  location            = azurerm_resource_group.vh_infra_core_ado.location
+  resource_group_name = azurerm_resource_group.vh_infra_core_ado.name
+
+  sku       = "Standard_D4s_v4"
+  instances = 1
+
+  admin_username = "adoagent"
+
+  source_image_id = "${azurerm_shared_image.ubuntu22_ado_agent.id}/versions/20230227.2.0"
+
+  admin_ssh_key {
+    username   = "adoagent"
+    public_key = tls_private_key.vmss.public_key_openssh
+  }
+
+  os_disk {
+    storage_account_type = "Premium_LRS"
+    caching              = "ReadWrite"
+  }
+
+  automatic_os_upgrade_policy {
+    enable_automatic_os_upgrade = true
+    disable_automatic_rollback  = true
+  }
+
+  network_interface {
+    name    = "${var.vmss_name}-nic"
+    primary = true
+
+    ip_configuration {
+      name      = "IpConfig"
+      primary   = true
+      subnet_id = azurerm_subnet.vh_infra_core_ado_snet.id
+    }
+  }
+
+  tags = local.common_tags
+}
