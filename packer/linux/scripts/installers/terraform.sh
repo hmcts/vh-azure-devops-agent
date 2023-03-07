613874a9579bc4da9d05b58f9f8997f9307c8d26
Your Name <james.ferrari@version1.com>
2023-03-07 10:35:13 +0000

    add install files for packer

---
+++ packer/linux/scripts/installers/terraform.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+################################################################################
+##  File:  terraform.sh
+##  Desc:  Installs terraform
+################################################################################
+
+source $HELPER_SCRIPTS/install.sh
+
+# Install Terraform
+apt install unzip -y 
+URL=$(curl -s https://api.releases.hashicorp.com/v1/releases/terraform/latest | jq -r '.builds[] | select((.arch=="amd64") and (.os=="linux")).url')
+ZIP_NAME="terraform_linux_amd64.zip"
+download_with_retries "${URL}" "/tmp" "${ZIP_NAME}"
+unzip -qq "/tmp/${ZIP_NAME}" -d /usr/local/bin
+rm -f "/tmp/${ZIP_NAME}"
+
+invoke_tests "Tools" "Terraform"
