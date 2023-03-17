85c74f2bcd00d9c6974f20ea392eec35010db662
Your Name <james.ferrari@version1.com>
2023-03-17 10:47:08 +0000

    amend fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
 module "ado_agent" {
   source = "git::https://github.com/hmcts/cnp-terraform-module-azure-devops-agent.git?ref=VIH-9741"
-  
+
   resource_group_name = var.rg_name
   location            = var.location
 
-  vnet_name           = var.vnet_name
-  vnet_address_space  = var.vnet_ip_address
+  vnet_name          = var.vnet_name
+  vnet_address_space = var.vnet_ip_address
 
   subnet_name           = var.subnet_name_vh_agent
   subnet_address_prefix = var.subnet_name_vh_agent_address
