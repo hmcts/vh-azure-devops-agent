901922f0daee5f23b8d9f056ed7df5fa53a3eb66
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2024-04-18 09:47:59 +0100

    Update azure-devops-agent.tf

--- terraform/azure-devops-agent.tf
+++ terraform/azure-devops-agent.tf
@@ -1,5 +1,5 @@
 module "agent" {
-  source = "git::https://github.com/hmcts/terraform-module-azure-devops-agent.git?ref=v1.0.0"
+  source = "git::https://github.com/hmcts/terraform-module-azure-devops-agent.git"
 
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
